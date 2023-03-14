e2e96bd54f5aae620ce5ed93eba132e60832e2fe
Alexander Villa <acvilla@bu.edu>
2023-03-14 00:49:44 -0700

    Update boot.py

--- boot.py
+++ boot.py
@@ -6,7 +6,7 @@ def connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('Harambe', 'FranzKafka17')
+        sta_if.connect('wifi', 'password')
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
